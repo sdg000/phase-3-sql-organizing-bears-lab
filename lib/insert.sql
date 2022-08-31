44bb00f1802d53b765b878bfe278eb46992d567f
Seth David Gyimah <esec26@gmail.com>
2022-08-31 11:51:25 +0000

    sql organizing bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears (
+    id,
+    name,
+    age,
+    sex,
+    color,
+    temperament,
+    alive
+
+)
+VALUES 
+    (null, "Mr. Chocolate", null, null, null, null, null),
+    (null, "Rowdy", null, null, null, null, null),
+    (null, "Tabitha", null, null, null, null, null),
+    (null, "Sergeant Brown", null, null, null, null, null),
+    (null, "Melissa", null, null, null, null, null),
+    (null, "Grinch", null, null, null, null, null),
+    (null, "Wendy", null, null, null, null, null),
+    (null, null, null, null, null, null, null);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,31 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  %Q(
+    select name, age from bears where sex = "F";
+  )
+  
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  %Q(
+    select name from bears order by name asc;
+  )
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  %Q(
+    select name, age from bears where alive is true order by age asc;
+  )
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  %Q(
+    select name, age from bears order by age desc limit 1
+  )
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  %Q(
+    select name, age from bears order by age asc limit 1
+  )
+
 end
